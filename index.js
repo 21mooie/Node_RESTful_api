71725c559cc37d530b24b3522f078103dd8f0b25
Muata Nkosi <mnkosi@fandm.edu>
2018-05-30 21:54:25 -0400

    adding config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
 
 var server = http.createServer(function(req,res){
      
@@ -63,8 +64,8 @@ var server = http.createServer(function(req,res){
 });
 
 
-server.listen(3000,function(){
-    console.log("The server is listening on port 3000");
+server.listen(config.port,function(){
+    console.log("The server is listening on port " + config.port + " in " + config.envName + " mode.");
 })
 
 var handlers = {};
